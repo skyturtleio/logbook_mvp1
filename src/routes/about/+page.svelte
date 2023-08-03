5d54fff13294da96aa51859b4e1f1199523cb0f2
Leo Mendoza Jr <dev@skyturtle.io>
2023-08-03 12:11:13 -0700

    change about to counter page

--- src/routes/about/+page.svelte
+++
@@ -1,3 +0,0 @@
-<h1>About Page</h1>
-
-<p>Testing time from changing code to deployment.</p>
---
+++ src/routes/counter/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+	let count = 0;
+
+	function increment() {
+		count += 1;
+	}
+
+	function reset() {
+		count = 0;
+	}
+</script>
+
+<h1>Counter</h1>
+
+<button on:click={increment}>
+	Clicked {count}
+	{count === 1 ? 'time' : 'times'}
+</button>
+
+<br />
+
+<button on:click={reset}> Reset to 0 </button>
